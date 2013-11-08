06f5bed343551430e4d73aa52184ca758586cb08
Rasmus Eneman <pie.or.paj@gmail.com>
2013-11-08 23:32:48 +0100

    Add method to create a Lamp

--- src/se/raxa/server/devices/helpers/Lamp.java
+++ src/se/raxa/server/devices/helpers/Lamp.java
@@ -1,11 +1,41 @@
 package se.raxa.server.devices.helpers;
 
+import se.raxa.server.exceptions.ClassCreationException;
 import se.raxa.server.exceptions.StatusChangeException;
 
 /**
  * @author Rasmus Eneman
  */
 public abstract class Lamp extends Output {
+
+    /**
+     * Create a new Lamp
+     *
+     * @param clazz Class of the Lamp
+     * @param name Name of the Lamp
+     * @param connector Connector of the lamp, may be null if appropriate
+     *
+     * @return A new Lamp object
+     *
+     * @throws ClassCreationException
+     */
+    public static <T extends Lamp> T create(Class<T> clazz, String name, Connector connector) throws
+            ClassCreationException {
+        try {
+            T obj = clazz.newInstance();
+            obj.setName(name);
+            obj.getDbObj().put("connector", connector.getDbObj());
+            obj.onCreate();
+            return obj;
+        } catch (InstantiationException | IllegalAccessException e) {
+            throw new ClassCreationException(String.format("Error when creating lamp from '%s'", clazz.getSimpleName()), e);
+        }
+    }
+
+    /**
+     * Called when a new object is created
+     */
+    protected void onCreate() {}
 
     /**
      * @return True if the lamp is turned on
