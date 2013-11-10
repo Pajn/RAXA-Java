bd190886d42e8afef5dc39ab93d099327291c149
Rasmus Eneman <pie.or.paj@gmail.com>
2013-11-10 13:14:06 +0100

    Test plugin names

---
+++ NexaSC/test/se/raxa/plugin/nexasc/tests/MainTest.java
@@ -0,0 +1,18 @@
+package se.raxa.plugin.nexasc.tests;
+
+import org.junit.Test;
+import se.raxa.plugin.nexasc.Main;
+import se.raxa.server.plugins.Plugin;
+
+import static junit.framework.Assert.assertEquals;
+
+/**
+ * @author Rasmus Eneman
+ */
+public class MainTest {
+    @Test
+    public void testGetName() throws Exception {
+        Plugin plugin = new Main();
+        assertEquals("Nexa Switch Case", plugin.getName());
+    }
+}
